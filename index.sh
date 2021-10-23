cffdc71d7e5272b7513d8bd5210a9fa87ba94e99
Nguyen Trung <nguyentrung204>
2021-10-23 15:52:02 +0700

    remove .github

---
+++ index.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# heroku git:remote -a 'store-cong-doi'
+# heroku buildpacks:set heroku/nodejs
+# heroku buildpacks:add heroku/python
+# heroku addons:create heroku-postgresql:hobby-dev
+# heroku addons:create heroku-redis:hobby-dev
+# heroku addons:create sendgrid:starter
+heroku config:set ALLOWED_HOSTS='https://admin-store-cong-doi.web.app'
+heroku config:set NODE_MODULES_CACHE=false
+heroku config:set NPM_CONFIG_PRODUCTION=false
+heroku config:set SECRET_KEY='4DAB6AEDF9C4AF97ECFC17A39D2C6'
